47baab2ec80501233758fd20d19a08f543eafb69
Steve Smith <sbs@stevenbsmith.net>
2021-03-02 10:57:11 -0500

    Add workplan for embeddings analysis

---
+++ OBL_embeddings_workflow.sh
@@ -0,0 +1,20 @@
+# generate input data & removing special set 
+
+# get/set iteration variables
+
+# run model with given parameters, including
+##model alroithim
+##input data 
+##hyperparameters
+##results output variables
+
+# store model performance (somehow)
+
+# run predictions (what wouldve been predicted vs new predictions)
+## all possible combinations - flag training
+## special set 
+
+# summary stats
+## score histogram, annotated by training, testing, valition, special set 
+
+
