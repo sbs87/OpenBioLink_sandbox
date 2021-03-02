e30bb905bc0d930361bd9ef2f0978cc03b5de93e
Steve Smith <sbs@stevenbsmith.net>
2021-03-02 12:58:23 -0500

    BIOG-69 add training command and paramteterize inputs

--- OBL_embeddings_workflow.sh
+++ OBL_embeddings_workflow.sh
@@ -13,6 +13,33 @@
 ##input data 
 ##hyperparameters
 ##results output variables
+MODEL="TransE_l2"
+DATA_PATH="/Users/stevensmith/Projects/OpenBioLink_sandbox/"
+TRAIN=DRUG_train_samples.csv
+TEST=DRUG_test_samples.csv 
+VAL=DRUG_val_samples.csv
+SAVE_PATH="/Users/stevensmith/Projects/OpenBioLink_sandbox/models/"
+
+dglke_train --model_name $MODEL \
+--format 'raw_udd_hrt'  \
+--data_path $DATA_PATH \
+--data_files $TRAIN $TEST $VAL \
+--batch_size 1000 \
+--neg_sample_size 200 \
+--hidden_dim 400 \
+--gamma 19.9 \
+--lr 0.25 \
+--max_step 500 \
+--log_interval 100 \
+--batch_size_eval 16 \
+-adv \
+--regularization_coef 1.00E-09  \
+--num_thread 1 \
+--num_proc 1 \
+--dataset OBL \
+--test \
+--valid \
+--save_path $SAVE_PATH
 
 # store model performance (somehow)
 
