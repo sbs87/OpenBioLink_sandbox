0452c08e30c9218f757cfb374c3834ef65b3891b
Steve Smith <sbs@stevenbsmith.net>
2021-03-04 14:52:03 -0500

    BIOG-69 move data pre and post processing to new script

--- OBL_embeddings_workflow.sh
+++ OBL_embeddings_workflow.sh
@@ -19,7 +19,7 @@ MODEL_NAME=$2
 DATA_PATH=$ROOT #"/Users/stevensmith/Projects/OpenBioLink_sandbox/"
 
 #TRAIN=SUBGRAPH_train_samples.csv 
-TRAIN=SUBGRAPH_train_samples.no_list1.tsv
+TRAIN=SUBGRAPH_train_samples.csv
 TEST=SUBGRAPH_test_samples.csv 
 VAL=SUBGRAPH_val_samples.csv
 ENTITY_MAP="entities.tsv"
@@ -27,29 +27,32 @@ REL_MAP="relations.tsv"
 SCORE_FUNC="logsigmoid"
 SAVE_PATH=$ROOT"/models/"
 MODEL_PATH=$SAVE_PATH""$MODEL_NAME"/"
+NODE1=node1
+NODE2=node2
+EDGE=edge
 
 PREDICT_OUT=$ROOT""$MODEL_NAME"_results.csv"
 
 
-dglke_train --model_name $MODEL \
---format 'raw_udd_hrt'  \
---data_path $DATA_PATH \
---data_files $TRAIN $TEST $VAL \
---batch_size 1000 \
---neg_sample_size 200 \
---hidden_dim 400 \
---gamma 19.9 \
---lr 0.25 \
---max_step 500 \
---log_interval 100 \
---batch_size_eval 16 \
--adv \
---regularization_coef 1.00E-09  \
---num_thread 1 \
---num_proc 1 \
---dataset OBL \
---test \
---save_path $SAVE_PATH
+# dglke_train --model_name $MODEL \
+# --format 'raw_udd_hrt'  \
+# --data_path $DATA_PATH \
+# --data_files $TRAIN $TEST $VAL \
+# --batch_size 1000 \
+# --neg_sample_size 200 \
+# --hidden_dim 400 \
+# --gamma 19.9 \
+# --lr 0.25 \
+# --max_step 500 \
+# --log_interval 100 \
+# --batch_size_eval 16 \
+# -adv \
+# --regularization_coef 1.00E-09  \
+# --num_thread 1 \
+# --num_proc 1 \
+# --dataset OBL \
+# --test \
+# --save_path $SAVE_PATH
 
 # store model performance (somehow)
 
@@ -72,16 +75,16 @@ dglke_train --model_name $MODEL \
 ## all possible combinations - flag training
 ## special set 
 
-# dglke_predict --model_path $MODEL_PATH  \
-# --format h_r_t \
-# --data_files node1 edge node2 \
-# --exec_mode 'triplet_wise' \
-# --topK 900 \
-# --score_func $SCORE_FUNC \
-# --raw_data \
-# --entity_mfile $ENTITY_MAP \
-# --rel_mfile $REL_MAP \
-# --output $PREDICT_OUT
+dglke_predict --model_path $MODEL_PATH  \
+--format h_r_t \
+--data_files $NODE1 $EDGE $NODE2 \
+--exec_mode 'triplet_wise' \
+--topK 100 \
+--score_func $SCORE_FUNC \
+--raw_data \
+--entity_mfile $ENTITY_MAP \
+--rel_mfile $REL_MAP \
+--output $PREDICT_OUT
 # summary stats
 ## score histogram, annotated by training, testing, valition, special set 
 
@@ -114,39 +117,3 @@ dglke_train --model_name $MODEL \
 
 #common name -> many IDs
 # lsit all combinations 
-
-
-# get DOID mapping 
-# rm -f preprocessing/map.*
-# awk -F '\t' '$3=="DO"' disease_mappings.tsv | grep -i parkin > preprocessing/map.parkinson
-# awk -F '\t' '$3=="DO"' disease_mappings.tsv | grep -i Diabetes > preprocessing/map.Diabetes
-# awk -F '\t' '$3=="DO"' disease_mappings.tsv | grep -i Malaria > preprocessing/map.Malaria
-# awk -F '\t' '$3=="DO"' disease_mappings.tsv | grep -i gout > preprocessing/map.gout
-# awk -F '\t' '$3=="DO"' disease_mappings.tsv | grep -i "Dermatitis herpetiformis" > preprocessing/map.Dermatitis
-
-
-
-
-# rm -f preprocessing/rel.*
-# cut -f4 preprocessing/map.Diabetes  | sort -u | awk '{print "DOID:"$0"\tDIS_DRUG\tPUBCHEM.COMPOUND:56843207"}' > preprocessing/rel.Diabetes_Colesevelam
-# cut -f4 preprocessing/map.parkinson  | sort -u | awk '{print "DOID:"$0"\tDIS_DRUG\tPUBCHEM.COMPOUND:31101"}' > preprocessing/rel.parkinson_Bromocriptine
-# cut -f4 preprocessing/map.Malaria  | sort -u | awk '{print "DOID:"$0"\tDIS_DRUG\tPUBCHEM.COMPOUND:2955"}' > preprocessing/rel.Malaria_Dapsone
-# cut -f4 preprocessing/map.gout  | sort -u | awk '{print "DOID:"$0"\tDIS_DRUG\tPUBCHEM.COMPOUND:135401907"}' > preprocessing/rel.gout_Allopurinol
-# cut -f4 preprocessing/map.Dermatitis  | sort -u | awk '{print "DOID:"$0"\tDIS_DRUG\tPUBCHEM.COMPOUND:2955"}' > preprocessing/rel.Dermatitis_Dapsone
-
-# cat preprocessing/rel.* > preprocessing/rel.all
-
-# cut 1, 2, 3 into node, edge,node OR rull all disease -< 1 drug (each drug) and filter out
-# also need to ensure inputs are in training. 
-
-
-# cut -f1 preprocessing/rel.all > preprocessing/node1.all
-# cut -f1 SUBGRAPH_train_samples.no_list1.tsv | grep DOID |sort -u > node1_training
-# grep -vwf node1_training preprocessing/node1.all  > node1_not_in_training
-# grep -vwf node1_not_in_training preprocessing/rel.all > preprocessing/rel.all.filter
-
-# cut -f3 preprocessing/rel.all > preprocessing/node2.all
-# cut -f3 SUBGRAPH_train_samples.no_list1.tsv | grep PUBCHEM |sort -u > node2_training
-# grep -vwf node2_training preprocessing/node2.all  > node2_not_in_training
-# grep -vwf node2_not_in_training preprocessing/rel.all.filter > tmp # 
-# preprocessing/rel.all.filter
